814ece07e99182620cdb16f7574d4f8286ee9ea8
rayzeng27 <alexylc@163.com>
2018-06-27 23:15:09 +0800

    添加代理配置

--- frontend/src/app/enum/InExType.enum.ts
+++ frontend/src/app/enum/InExType.enum.ts
@@ -1,14 +1,14 @@
 export enum EnumInExType
 {
     /** 收支类型:转移 - 用于记录在不同的两个账户之间进行资金转移的操作，不涉及收支 */
-    NONE,
+    NONE = "NONE",
 
     /** 收支类型:收入 */
-    INCOME,
+    INCOME = "INCOME",
 
     /** 收支类型:支出 */
-    EXPENSES,
+    EXPENSES = "EXPENSES",
 
     /** 收支类型：注释 - 用于记录一些生活中跟钱有关的信息，但即不涉及收支，也没有各种强校验检查 */
-    COMMENT
+    COMMENT = "COMMENT"
 }
