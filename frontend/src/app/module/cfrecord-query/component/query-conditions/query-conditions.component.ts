d4297a6aafc905b438943f72fb1214c1916f02df
rayzeng27 <alexylc@163.com>
2018-07-18 23:20:49 +0800

    添加Reactive代码片段

--- frontend/src/app/module/cfrecord-query/component/query-conditions/query-conditions.component.ts
+++ frontend/src/app/module/cfrecord-query/component/query-conditions/query-conditions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { EnumInExType } from '../../../../enum/inex-type.enum';
 import { AccountService } from '../../../../service/account.service';
@@ -30,9 +30,184 @@ export class QueryConditionsComponent implements OnInit
                 enable : false,
                 lowerLimit : 0,
                 upperLimit : 0
+            }),
+            inExCondition : this.fb.group({
+                enable : false,
+                inExType : EnumInExType.EXPENSES,
+                inExCategories : []
+            }),
+            accountCondition : this.fb.group({
+                enable : false,
+                accounts : []
+            }),
+            tagCondition : this.fb.group({
+                enable : false,
+                tags : []
+            }),
+            dateTimeCondition : this.fb.group({
+                enable : false,
+                dateRange : null
+            }),
+            ownerCondition : this.fb.group({
+                enable : false,
+                owners : []
+            }),
+            remarkCondition : this.fb.group({
+                enable : false,
+                remark : ""
             })
         });
 
+        this.initMoneyConditionUI();
+        this.initInExConditionUI();
+        this.initAccountConditionUI();
+        this.initTagConditionUI();
+        this.initDateTimeConditionUI();
+        this.initOwnerConditionUI();
+        this.initRemarkConditionUI();
+
         this.accountGroups = this.accountService.getAccountGroups();
+    }
+
+    private initMoneyConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("moneyCondition.enable");
+        let lowerLimitControl = this.conditionForm.get("moneyCondition.lowerLimit");
+        let upperLimitControl = this.conditionForm.get("moneyCondition.upperLimit");
+
+        lowerLimitControl.disable();
+        upperLimitControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                lowerLimitControl.enable();
+                upperLimitControl.enable();
+            }
+            else
+            {
+                lowerLimitControl.disable();
+                upperLimitControl.disable();
+            }
+        });
+    }
+
+    private initInExConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("inExCondition.enable");
+        let inExTypeControl = this.conditionForm.get("inExCondition.inExType");
+        let inExCategoriesControl = this.conditionForm.get("inExCondition.inExCategories");
+
+        inExTypeControl.disable();
+        inExCategoriesControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                inExTypeControl.enable();
+                inExCategoriesControl.enable();
+            }
+            else
+            {
+                inExTypeControl.disable();
+                inExCategoriesControl.disable();
+            }
+        });
+    }
+
+    private initAccountConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("accountCondition.enable");
+        let accountsControl = this.conditionForm.get("accountCondition.accounts");
+
+        accountsControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                accountsControl.enable();
+            }
+            else
+            {
+                accountsControl.disable();
+            }
+        });
+    }
+
+    private initTagConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("tagCondition.enable");
+        let tagsControl = this.conditionForm.get("tagCondition.tags");
+
+        tagsControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                tagsControl.enable();
+            }
+            else
+            {
+                tagsControl.disable();
+            }
+        });
+    }
+
+    private initDateTimeConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("dateTimeCondition.enable");
+        let dateRangeControl = this.conditionForm.get("dateTimeCondition.dateRange");
+
+        dateRangeControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                dateRangeControl.enable();
+            }
+            else
+            {
+                dateRangeControl.disable();
+            }
+        });
+    }
+
+    private initOwnerConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("ownerCondition.enable");
+        let ownersControl = this.conditionForm.get("ownerCondition.owners");
+
+        ownersControl.disable();
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                ownersControl.enable();
+            }
+            else
+            {
+                ownersControl.disable();
+            }
+        });
+    }
+
+    private initRemarkConditionUI() : void
+    {
+        let enableControl = this.conditionForm.get("remarkCondition.enable");
+        let remarkControl = this.conditionForm.get("remarkCondition.remark");
+
+        remarkControl.disable();
+        remarkControl.setValidators(Validators.required);
+
+        enableControl.valueChanges.subscribe(enable => {
+            if (enable)
+            {
+                remarkControl.enable();
+            }
+            else
+            {
+                remarkControl.disable();
+            }
+        });
     }
 }
